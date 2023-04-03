92758f3c3292c76ad14e54b8aec650aef32cadf8
Gustavo Gava <gustavogava123@hotmail.com>
2023-04-03 14:41:38 -0300

    refactor: moved InformativeContent result to a component

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+// export all components here
+export * from './Footer';
+export * from './Header';
+export * from './Home/Form';
+export * from './Home/InformativeContent'
+export * from './Home/FormResult';
