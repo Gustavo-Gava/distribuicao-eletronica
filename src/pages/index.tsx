ce206029d9d495d1d91b3ca0f24b1bb0046dd7de
Gustavo Gava <gustavogava123@hotmail.com>
2023-04-03 14:41:50 -0300

    refactor: refactored the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,9 @@
-import { Header } from "@/components/header";
-import { Form } from "@/components/home/Form";
+import { Footer, Form, FormResult, Header, InformativeContent } from "@/components";
 import { useChemicalElement } from "@/context/ChemicalElementProvider";
-import { getSubnivelString } from "@/getSubnivelString";
 import Head from "next/head";
-import Image from "next/image";
-
-const element_properties_dictionary = {
-	nome: "Nome do elemento",
-	abr: "Abreviação",
-	massa: "Massa atômica",
-	num_atomico: "Número atômico",
-	num_protons: "Número de prótons",
-	num_neutrons: "Número de nêutrons",
-} as Record<string, string>;
-
-const banned_properties = ["familia", "periodo"];
 
 export default function Home() {
-	const { element, layerDistribution, sublevelDistribution } = useChemicalElement();
-
-	const sublevelDistributionFormatted = getSubnivelString(sublevelDistribution) ?? "";
+	const { element } = useChemicalElement();
 
 	return (
 		<>
@@ -39,130 +23,13 @@ export default function Home() {
 				<div className="m-auto">
 					<Form />
 
-					{element?.nome && (
-						<>
-							<div className="my-6 h-[1px] w-full bg-gray-200 shadow-md" />
-
-							<div className="flex flex-col justify-between gap-8 md:flex-row">
-								<div className="flex w-full flex-col gap-2 rounded-lg bg-blue-400 bg-opacity-50 p-4 shadow-lg">
-									<h3 className="text-xl font-medium">Classificação por camadas</h3>
-
-									<div>
-										{Object.entries(layerDistribution).map(([key, value]) => {
-											if (value === 0) return null;
-
-											return (
-												<p key={key}>
-													{key.toUpperCase()}: {value}
-												</p>
-											);
-										})}
-									</div>
-								</div>
-
-								<div className="flex w-full flex-col gap-2 rounded-lg bg-blue-400 bg-opacity-50 p-4 shadow-lg">
-									<h3 className="text-xl font-medium">Classificação por subníveis</h3>
-
-									<div
-										className="flex gap-2"
-										dangerouslySetInnerHTML={{ __html: sublevelDistributionFormatted }}
-									/>
-								</div>
-							</div>
-
-							<div className="mt-6 flex w-full flex-col gap-2 rounded-lg bg-blue-400 bg-opacity-50 p-4 shadow-lg">
-								<h3 className="text-xl font-medium">Propriedades do Elemento</h3>
-
-								<div>
-									{Object.entries(element).map(([key, value], index) => {
-										if (value === null || value === 0 || banned_properties.includes(key))
-											return null;
-
-										return (
-											<p key={index}>
-												{element_properties_dictionary[key]}: {value}
-											</p>
-										);
-									})}
-								</div>
-							</div>
-						</>
-					)}
+					{element?.nome && <FormResult />}
 				</div>
 
-				<div className="mt-36 flex flex-col gap-6">
-					<div>
-						<h3 className="text-xl font-medium">O que é a distribuição eletrônica?</h3>
-
-						<p>
-							A distribuição eletrônica representa a forma como os elétrons se organizam na
-							eletrosfera do elemento químico.
-						</p>
-					</div>
-
-					<div>
-						<h3 className="text-xl font-medium">Como é a estrutura do átomo?</h3>
-
-						<div>
-							<p>
-								O <strong>átomo</strong> é dividido basicamente em duas regiões: O{" "}
-								<strong>núcleo</strong> e a<strong>eletrosfera.</strong>
-							</p>
-
-							<p>
-								O <strong>núcleo</strong> é composto pelos{" "}
-								<strong className="text-green-600">prótons</strong> (carga <span>positiva</span>) e
-								os <strong className="text-gray-400">nêutrons</strong> (carga <span>neutra</span>).
-								Os nêutrons são essenciais no núcleo do átomo para separar os prótons, pois
-								elementos com cargas iguais se repelem, então jamais conseguiriam permanecer juntos
-								no núcleo atômico. O núcleo também é responsável por aproximadamente toda massa do
-								átomo (em vista de que a massa da eletrosfera é insignificante.)
-							</p>
-
-							<p>
-								A <strong>eletrosfera</strong> consiste em um espaço majoritariamente vazio (este
-								que é aproximadamente 100.000 vezes o tamanho do núcleo) que comporta os elétrons do
-								átomo, que possuem cargas <span>negativas.</span> Os elétrons nessa eletrosfera são
-								divididos em camadas, como podemos ver na imagem abaixo:
-								<Image
-									src="/images/camadas_atomo.webp"
-									width={300}
-									height={300}
-									alt="Camadas do átomo"
-								/>
-							</p>
-						</div>
-					</div>
-
-					<div>
-						<h3 className="text-xl font-medium">Diagrama de Pauling</h3>
-
-						<div>
-							<p>
-								Anteriormente a Pauling, sabia-se que os elétrons eram distribuídos em camadas,
-								porém não havia conhecimento de como eles se organizavam dentro dessas camadas.
-								Pauling conseguiu com seus estudos, descobrir que a forma com que os elétrons se
-								organizavam eram em subníveis, e daí se criou o famoso Diagrama de Linus Pauling.
-							</p>
-							<p>
-								Ao usar o Diagrama de Pauling, um dos erros mais cometidos é de seguir
-								horizontalmente a ordem, sendo que a forma correta seria de acordo com a imagem
-								abaixo:
-								<Image
-									src="/images/diagrama_linus_pauling.png"
-									width={300}
-									height={300}
-									alt="Diagrama Linus Pauling"
-								/>
-							</p>
-						</div>
-					</div>
-				</div>
+				<InformativeContent />
 			</main>
 
-			<footer className="bottom-0 flex w-full items-center justify-center bg-blue-500 p-4 text-white">
-				<p>Copyright © Gustavo Gava 2021</p>
-			</footer>
+			<Footer />
 		</>
 	);
 }
