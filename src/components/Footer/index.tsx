d6f9a5533739cf307ac52be9f7bba0576cb394c7
Gustavo Gava <gustavogava123@hotmail.com>
2023-04-03 14:41:11 -0300

    refactor: moved Footer to a component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+	return (
+		<footer className="bottom-0 flex w-full items-center justify-center bg-blue-500 p-4 text-white">
+			<p>Copyright © Gustavo Gava 2021</p>
+		</footer>
+	);
+}
