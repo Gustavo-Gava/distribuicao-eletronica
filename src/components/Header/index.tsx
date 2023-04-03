21410b4f187fc24611d69cf080f212ce101f8c91
Gustavo Gava <gustavogava123@hotmail.com>
2023-04-03 15:00:42 -0300

    chore: changed git config ignorecase to false

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+	return (
+		<header className="flex items-center justify-center bg-blue-600 p-8">
+			<h1 className="text-2xl text-white">Calculadora de Distribuição Eletrônica</h1>
+		</header>
+	);
+}
