f1db85fa69135c0d6fce1226f40842922402ca44
Gustavo Gava <gustavogava123@hotmail.com>
2023-04-03 14:53:44 -0300

    fix: removed shadow of header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,6 @@
 export function Header() {
 	return (
-		<header className="flex items-center justify-center bg-blue-600 p-8 shadow-xl">
+		<header className="flex items-center justify-center bg-blue-600 p-8">
 			<h1 className="text-2xl text-white">Calculadora de Distribuição Eletrônica</h1>
 		</header>
 	);
